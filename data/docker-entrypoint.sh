dbefe4bd969883697f24f5e00923f2874baabb55
bhw1994 <bhw1994@gmail.com>
2019-05-01 15:20:57 +0900

    modify path

--- data/docker-entrypoint.sh
+++ data/docker-entrypoint.sh
@@ -3,12 +3,12 @@ set -e
  
 ## from redis-5.0
 # Redis서버에 접근가능한 Host를 설정합니다.
-sed -i "s/bind 127.0.0.1/bind $CLIENTHOST 127.0.0.1/g" /data/redis.conf
+sed -i "s/bind 127.0.0.1/bind $CLIENTHOST 127.0.0.1/g" /boyj/redis.conf
 
 ### redis port inside redis.conf
 #redis.conf 파일의 port 6379 문자열을 port $CLINETPORT로 변경합니다. 
 #($CLIENTPORT는 docker-compose.yml파일의 Environment에서 지정하였습니다.)
-sed -i "s/port 6379/port $CLIENTPORT/g" /data/redis.conf 
+sed -i "s/port 6379/port $CLIENTPORT/g" /boyj/redis.conf 
 
 # requirepass foobared 문자열을 requirepass $REQUIREPASS로 변경하였습니다. 
 #(Redis 접속 시 비밀번호를 설정하였습니다.)
@@ -18,7 +18,7 @@ sed -i "s/port 6379/port $CLIENTPORT/g" /data/redis.conf
 ### slaveof <masterip> <masterport> => slaveof $MASTERHOST $MASTERPORT
 # $MASTERPORT가 공백이 아닌 Container에 대해 slaveof 설정을 통해 Master-Slave 설정을 합니다.
 if [ "$MASTERPORT" != "" ];then
-    sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /data/redis.conf  
+    sed -i "s/# slaveof <masterip> <masterport>/slaveof $MASTERHOST $MASTERPORT/g" /boyj/redis.conf  
 fi
  
 # first arg is `-f` or `--some-option`
